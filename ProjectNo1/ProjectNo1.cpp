f3c86346468e3e1d316f5695898f2a87c35b5be1
xodnr7727 <70584570+xodnr7727@users.noreply.github.com>
2023-08-28 13:18:45 +0900

    Delete ProjectNo1 directory

--- ProjectNo1/ProjectNo1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjectNo1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectNo1, "ProjectNo1" );
- 
